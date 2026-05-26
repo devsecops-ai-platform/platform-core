01a991ca1230c7eff93e6f1aae484467a5eef29c
thahir2005 <skthahir2005@gmail.com>
2026-05-26 14:39:12 +0530

    Fix SQLAlchemy model registration

--- services/auth-service/app.py
+++ services/auth-service/app.py
@@ -1,6 +1,21 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
+from dotenv import load_dotenv
+import os
+
+from app.config.db import db, DATABASE_URI
+from app.models.user_model import User
+
+load_dotenv()
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = os.getenv("JWT_SECRET_KEY")
+
+db.init_app(app)
+jwt = JWTManager(app)
 
 @app.route('/')
 def home():
@@ -8,6 +23,9 @@ def home():
         "service": "auth-service",
         "status": "running"
     }
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
---
+++ services/auth-service/app/config/db.py
@@ -0,0 +1,18 @@
+import os
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db = SQLAlchemy()
+
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+DATABASE_URI = (
+    f"postgresql://{DB_USER}:{DB_PASSWORD}"
+    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+)
